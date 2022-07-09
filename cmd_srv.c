b64840d561d5d4007d511f8624f899f255590c1f
roleo <roleo@iperbole.bologna.it>
2022-07-09 17:46:56 +0200

    Change mode to input parameters and add functions

--- cmd_srv.c
+++ cmd_srv.c
@@ -158,11 +158,61 @@ int p2p_send_msg(mqd_t mqfd, MSG_TYPE msg_type, char *payload, int payload_len)
     return fsMsgRet;
 }
 
+/* Only positive numbers */
+int str2int(char *value)
+{
+    char *endptr;
+    int errno;
+    int ret;
+
+    errno = 0;    /* To distinguish success/failure after call */
+
+    ret = strtol(value, &endptr, 10);
+
+    /* Check for various possible errors */
+    if ((errno == ERANGE && (ret == LONG_MAX || ret == LONG_MIN)) || (errno != 0)) {
+        return -1;
+    }
+    if (endptr == optarg) {
+        return -1;
+    }
+
+    return ret;
+}
+
+/* Only positive numbers */
+int str2motion_rect(motion_rect_t *mr, char *value)
+{
+    char *p = strtok (value, ",");
+    int el;
+    int array[6];
+    int i;
+
+    while (p != NULL)
+    {
+        el = str2int(p);
+        if (el == -1) return -1;
+        array[i++] = el;
+        p = strtok (NULL, ".");
+    }
+    mr->mode = array[0];
+    mr->resolution = array[1];
+    mr->left = array[2];
+    mr->top = array[3];
+    mr->right = array[4];
+    mr->bottom = array[5];
+
+}
+
 void print_usage(char *progname)
 {
     fprintf(stderr, "\nUsage: %s OPTIONS\n\n", progname);
-    fprintf(stderr, "\t-m MIC_VOLUME, --mic_volume MIC_VOLUME\n");
-    fprintf(stderr, "\t\tset mic volume\n");
+    fprintf(stderr, "\t-s PARAM, --set PARAM\n");
+    fprintf(stderr, "\t\tset PARAM to value\n");
+    fprintf(stderr, "\t-g PARAM, --get PARAM\n");
+    fprintf(stderr, "\t\tget PARAM value\n");
+    fprintf(stderr, "\t-v VALUE, --value VALUE\n");
+    fprintf(stderr, "\t\tvalue to set\n");
     fprintf(stderr, "\t-d, --debug\n");
     fprintf(stderr, "\t\tenable debug\n");
     fprintf(stderr, "\t-h, --help\n");
@@ -175,13 +225,18 @@ int main(int argc, char **argv)
     char *endptr;
     int c;
 
-    int mic_volume = -1;
+    int set = 0;
+    char param[1024];
+    char value[1024] = {0};
+    int ivalue;
     int debug = 0;
 
     while (1) {
         static struct option long_options[] =
         {
-            {"mic_volume",  required_argument, 0, 'm'},
+            {"set",  required_argument, 0, 's'},
+            {"get",  required_argument, 0, 'g'},
+            {"value",  required_argument, 0, 'v'},
             {"debug",  no_argument, 0, 'd'},
             {"help",  no_argument, 0, 'h'},
             {0, 0, 0, 0}
@@ -189,7 +244,7 @@ int main(int argc, char **argv)
         /* getopt_long stores the option index here. */
         int option_index = 0;
 
-        c = getopt_long (argc, argv, "m:dh",
+        c = getopt_long (argc, argv, "s:g:v:ldh",
                          long_options, &option_index);
 
         /* Detect the end of the options. */
@@ -197,19 +252,53 @@ int main(int argc, char **argv)
             break;
 
         switch (c) {
-        case 'm':
-            errno = 0;    /* To distinguish success/failure after call */
-            mic_volume = strtol(optarg, &endptr, 10);
+        case 's':
+        case 'g':
+            if (c == 's') set = 1;
+            if (strlen(optarg) < sizeof(param) - 1) {
+                strcpy(param, optarg);
+            } else {
+                print_usage(argv[0]);
+                exit(EXIT_FAILURE);
+            }
+            break;
 
-            /* Check for various possible errors */
-            if ((errno == ERANGE && (mic_volume == LONG_MAX || mic_volume == LONG_MIN)) || (errno != 0)) {
+//            errno = 0;    /* To distinguish success/failure after call */
+//
+//            mic_volume = strtol(optarg, &endptr, 10);
+//
+//            /* Check for various possible errors */
+//            if ((errno == ERANGE && (mic_volume == LONG_MAX || mic_volume == LONG_MIN)) || (errno != 0)) {
+//                print_usage(argv[0]);
+//                exit(EXIT_FAILURE);
+//            }
+//            if (endptr == optarg) {
+//                print_usage(argv[0]);
+//                exit(EXIT_FAILURE);
+//            }
+//            break;
+
+        case 'v':
+            if (strlen(optarg) < sizeof(value) - 1) {
+                strcpy(value, optarg);
+            } else {
                 print_usage(argv[0]);
                 exit(EXIT_FAILURE);
             }
-            if (endptr == optarg) {
-                print_usage(argv[0]);
-                exit(EXIT_FAILURE);
-            }
+            break;
+
+        case 'l':
+            printf("List of parameters:\n");
+            printf("\tmic_volume\n");
+            printf("\tpower_mode\n");
+            printf("\tlight_mode\n");
+            printf("\trecord_mode\n");
+            printf("\tmirror\n");
+            printf("\tmotion_rect\n");
+            printf("\thuman_motion_enable\n");
+            printf("\tirlight_mode\n");
+            printf("\tmotion_sensitivity\n");
+            exit(EXIT_SUCCESS);
             break;
 
         case 'd':
@@ -251,8 +340,100 @@ int main(int argc, char **argv)
     }
     printf("Size of mmap.info: %d\n", sizeof(mmap_info_s));
 
-    if (mic_volume != -1) {
-        p2p_set_mic_volume(mic_volume);
+    if (set == 1) {
+        if (strcmp("mic_volume", param) == 0) {
+            ivalue = str2int(value);
+            if (ivalue == -1) {
+                printf("Invalid value: %s\n", value);
+                return -2;
+            }
+            p2p_set_mic_volume(ivalue);
+        } else if (strcmp("power_mode", param) == 0) {
+            ivalue = str2int(value);
+            if (ivalue == -1) {
+                printf("Invalid value: %s\n", value);
+                return -2;
+            }
+            p2p_set_power(ivalue);
+        } else if (strcmp("light_mode", param) == 0) {
+            ivalue = str2int(value);
+            if (ivalue == -1) {
+                printf("Invalid value: %s\n", value);
+                return -2;
+            }
+            p2p_set_light(ivalue);
+        } else if (strcmp("record_mode", param) == 0) {
+            ivalue = str2int(value);
+            if (ivalue == -1) {
+                printf("Invalid value: %s\n", value);
+                return -2;
+            }
+            p2p_set_motion_record(ivalue);
+        } else if (strcmp("mirror", param) == 0) {
+            ivalue = str2int(value);
+            if (ivalue == -1) {
+                printf("Invalid value: %s\n", value);
+                return -2;
+            }
+            if (ivalue == 1)
+                p2p_set_mirror_flip(DISPATCH_SET_MIRROR_ON);
+            else
+                p2p_set_mirror_flip(DISPATCH_SET_MIRROR_OFF);
+
+        } else if (strcmp("motion_rect", param) == 0) {
+            motion_rect_t motion_rect;
+            ivalue = str2motion_rect(&motion_rect, value);
+            if (ivalue == -1) {
+                printf("Invalid value: %s\n", value);
+                return -2;
+            }
+            p2p_set_motion_detect(motion_rect);
+        } else if (strcmp("human_motion_enable", param) == 0) {
+            ivalue = str2int(value);
+            if (ivalue == -1) {
+                printf("Invalid value: %s\n", value);
+                return -2;
+            }
+            p2p_set_alarm_mode(ivalue);
+        } else if (strcmp("irlight_mode", param) == 0) {
+            ivalue = str2int(value);
+            if (ivalue == -1) {
+                printf("Invalid value: %s\n", value);
+                return -2;
+            }
+            p2p_set_day_night_mode(ivalue);
+        } else if (strcmp("motion_sensitivity", param) == 0) {
+            ivalue = str2int(value);
+            if (ivalue == -1) {
+                printf("Invalid value: %s\n", value);
+                return -2;
+            }
+            p2p_set_alarm_sensitivity(ivalue);
+        } else {
+            printf("Invalid parameter: %s\n", param);
+        }
+    } else {
+        if (strcmp("mic_volume", param) == 0) {
+            p2p_get_mic_volume();
+        } else if (strcmp("power_mode", param) == 0) {
+            p2p_get_power();
+        } else if (strcmp("light_mode", param) == 0) {
+            p2p_get_light();
+        } else if (strcmp("record_mode", param) == 0) {
+            p2p_get_motion_record();
+        } else if (strcmp("mirror", param) == 0) {
+            p2p_get_mirror_flip();
+        } else if (strcmp("motion_rect", param) == 0) {
+            p2p_get_motion_detect();
+        } else if (strcmp("human_motion_enable", param) == 0) {
+            p2p_get_alarm_mode();
+        } else if (strcmp("irlight_mode", param) == 0) {
+            p2p_get_day_night_mode();
+        } else if (strcmp("motion_sensitivity", param) == 0) {
+            p2p_get_alarm_sensitivity();
+        } else {
+            printf("Invalid parameter: %s\n", param);
+        }
     }
 
     mq_close(ipc_mq);
